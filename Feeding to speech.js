225020cb618f493725ce7b529af2520fceb1b79e
Arun Kumar <rarunkumar15112k@gmail.com>
2020-11-02 18:02:51 +0530

    Feeding to speech.js

--- Feeding to speech.js
+++ Feeding to speech.js
@@ -1,2 +1,3 @@
+//Extracting the classes and the probability score from 'msg'(The Output given by the Watson Visual Recognition Service).
 msg.payload=msg.result.images[0].classifiers[0].classes
 return msg;
